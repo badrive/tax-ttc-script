b47c4459bb3a58b335db65d59b153e69f6736e03
cd-badr <badr@gmail.com>
2024-03-25 18:20:47 +0000

    float

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 let btn = document.querySelector("#btn");
 let price = document.querySelector("#price");
 let tax = document.querySelector("#tax");
-
-let type = document.getElementById("type");
+let tableRow = document.getElementById('myTable').getElementsByTagName('tr')[1];
 
 btn.addEventListener('click', () => {
+    let newTd = document.createElement('td');
 
     let text1 = type.options[type.selectedIndex].text;
     document.getElementById("result").innerText = text1;
@@ -15,7 +15,24 @@ btn.addEventListener('click', () => {
     const TaxValue = tax.value;
     document.getElementById("result3").innerText = TaxValue;
 
-    document.getElementById("result4").innerText = ((parseInt(PriceValue) * TaxValue) / 100 ) + parseInt(PriceValue);
+    document.getElementById("result4").innerText = ((parseFloat(PriceValue) * TaxValue) / 100 ) + parseFloat(PriceValue);
 
+    newTd.textContent = PriceValue;
+    tableRow.appendChild(newTd);
+
+    
+    // let x = document.createElement("td");
+    // x.setAttribute("value", PriceValue); 
+    // tableRow.appendChild(x);
+
+    // let a = document.createElement("td");
+    // a.setAttribute("value", TaxValue); 
+    // tableRow.appendChild(a);
+
+    // let b = document.createElement("td");
+    // b.setAttribute("value", TaxValue); 
+    // tableRow.appendChild(b);
+
+    
 });
 
